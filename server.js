8e7d3ed6e8e0589acab758ead2089f032937d388
MatheusNicolasTz <matheuspcgamer4@gmail.com>
2024-06-26 12:02:10 -0300

    Initial commit

--- server.js
+++ server.js
@@ -1,15 +1,37 @@
 const express = require('express');
+const http = require('http');
 const path = require('path');
+const socketIo = require('socket.io');
 
 const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
 const PORT = process.env.PORT || 3000;
 
+// Serve arquivos estáticos da pasta 'public'
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-app.listen(PORT, () => {
+io.on('connection', (socket) => {
+    console.log('a user connected');
+    
+    socket.on('drawing', (data) => {
+        socket.broadcast.emit('drawing', data);
+    });
+
+    socket.on('fill', (data) => {
+        socket.broadcast.emit('fill', data);
+    });
+
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
+});
+
+server.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
